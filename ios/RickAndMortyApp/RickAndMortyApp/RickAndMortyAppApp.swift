4a2257f2e55aed447dd9ca40dc8f8b1f9babbf1c
Efe Akyurt <efeakyurt@192.168.1.104>
2025-02-23 15:53:37 +0300

    chore: Add all project files

---
+++ ios/RickAndMortyApp/RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Efe Akyurt on 23.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/RickAndMortyApp/RickAndMortyAppTests/RickAndMortyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppTests.swift
+//  RickAndMortyAppTests
+//
+//  Created by Efe Akyurt on 23.02.2025.
+//
+
+import Testing
+@testable import RickAndMortyApp
+
+struct RickAndMortyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
